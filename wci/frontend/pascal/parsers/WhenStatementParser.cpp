0f5b83298bd984d06f58d4097d8d324c40cfa12e
alexn <alex.norell@gmail.com>
2017-09-29 17:27:05 -0700

    Fixed spacing in whenstatementparsers.cpp and added nested ifs, maybe

--- wci/frontend/pascal/parsers/WhenStatementParser.cpp
+++ wci/frontend/pascal/parsers/WhenStatementParser.cpp
@@ -22,128 +22,128 @@
 
 namespace wci { namespace frontend { namespace pascal { namespace parsers {
 
-                using namespace std;
-                using namespace wci::frontend::pascal;
-                using namespace wci::intermediate;
-                using namespace wci::intermediate::icodeimpl;
+    using namespace std;
+    using namespace wci::frontend::pascal;
+    using namespace wci::intermediate;
+    using namespace wci::intermediate::icodeimpl;
 
-                bool WhenStatementParser::INITIALIZED = false;
+    bool WhenStatementParser::INITIALIZED = false;
 
-                set<PascalTokenType> WhenStatementParser::ARROW_SET;
+    set<PascalTokenType> WhenStatementParser::ARROW_SET;
 
-                void WhenStatementParser::initialize()
+    void WhenStatementParser::initialize()
+    {
+        if (INITIALIZED) return;
+
+        ARROW_SET = StatementParser::STMT_START_SET;
+        ARROW_SET.insert(PascalTokenType::ARROW);
+
+        set<PascalTokenType>::iterator it;
+        for (it  = StatementParser::STMT_FOLLOW_SET.begin();
+             it != StatementParser::STMT_FOLLOW_SET.end();
+             it++)
+        {
+            ARROW_SET.insert(*it);
+        }
+
+        INITIALIZED = true;
+    }
+
+    WhenStatementParser::WhenStatementParser(PascalParserTD *parent)
+            : StatementParser(parent)
+    {
+        initialize();
+    }
+
+    ICodeNode *WhenStatementParser::parse_statement(Token *token) throw (string)
+    {
+        token = next_token(token);  // consume the WHEN
+
+        // Create an IF node.
+        ICodeNode *if_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_IF);
+
+        // Parse the expression.
+        // The IF node adopts the expression subtree as its first child.
+        ExpressionParser expression_parser(this);
+        if_node->add_child(expression_parser.parse_statement(token));
+
+        // Synchronize at the ARROW.
+        token = synchronize(ARROW_SET);
+
+        if (token->get_type() == (TokenType) PT_ARROW)
+        {
+            token = next_token(token);  // consume the ARROW
+        }
+        else {
+            // TODO: Add error handling for ARROW
+            error_handler.flag(token, MISSING_THEN, this);
+        }
+
+        // Parse the compound statement.
+        // The IF node adopts the statement subtree as its second child.
+        StatementParser statement_parser(this);
+        if_node->add_child(statement_parser.parse_statement(token));
+        token = current_token();
+
+        token = next_token(token); // EAT THE FUCKING SEMICOLON
+
+        /
+
+        // TODO: Move this into a function that takes parent if node and add the results to its third child
+        // Currently not working because original IF node has too many children
+        while(token->get_type() != (TokenType) PT_END && token->get_type() != (TokenType) PT_OTHERWISE)
+        {
+            // Parse expression
+            // The IF node adopts the expression subtree as its first child.
+            if_node->add_child(expression_parser.parse_statement(token));
+            if_node = if_node->get_children()[2];
+
+            // Synchronize at the ARROW.
+            token = synchronize(ARROW_SET);
+
+            if (token->get_type() == (TokenType) PT_ARROW)
+            {
+                token = next_token(token);  // consume the ARROW
+            }
+            else {
+                // TODO: Add error handling for ARROW
+                error_handler.flag(token, MISSING_THEN, this);
+            }
+
+            // Parse the compound statement.
+            // The IF node adopts the statement subtree as its second child.
+            if_node->add_child(statement_parser.parse_statement(token));
+            token = current_token();
+
+            token = next_token(token); // EAT THE FUCKING SEMICOLON
+
+            // Look for an OTHERWISE.
+            if (token->get_type() == (TokenType) PT_OTHERWISE)
+            {
+                token = next_token(token);  // consume the OTHERWISE
+
+                // Synchronize at the ARROW.
+                token = synchronize(ARROW_SET);
+
+                if (token->get_type() == (TokenType) PT_ARROW)
                 {
-                    if (INITIALIZED) return;
-
-                    ARROW_SET = StatementParser::STMT_START_SET;
-                    ARROW_SET.insert(PascalTokenType::ARROW);
-
-                    set<PascalTokenType>::iterator it;
-                    for (it  = StatementParser::STMT_FOLLOW_SET.begin();
-                         it != StatementParser::STMT_FOLLOW_SET.end();
-                         it++)
-                    {
-                        ARROW_SET.insert(*it);
-                    }
-
-                    INITIALIZED = true;
+                    token = next_token(token);  // consume the ARROW
+                }
+                else {
+                    // TODO: Add error handling for ARROW
+                    error_handler.flag(token, MISSING_THEN, this);
                 }
 
-                WhenStatementParser::WhenStatementParser(PascalParserTD *parent)
-                        : StatementParser(parent)
-                {
-                    initialize();
-                }
+                // Parse the compound statement.
+                // The IF node adopts the statement subtree as its third child.
+                if_node->add_child(statement_parser.parse_statement(token));
 
-                ICodeNode *WhenStatementParser::parse_statement(Token *token) throw (string)
-                {
-                    token = next_token(token);  // consume the WHEN
+            }
+        }
+        if_node = temp_if_node;
+        token = next_token(token); // consume END
 
-                    // Create an IF node.
-                    ICodeNode *if_node =
-                            ICodeFactory::create_icode_node((ICodeNodeType) NT_IF);
+        return if_node;
+    }
 
-
-                    // Parse the expression.
-                    // The IF node adopts the expression subtree as its first child.
-                    ExpressionParser expression_parser(this);
-                    if_node->add_child(expression_parser.parse_statement(token));
-
-                    // Synchronize at the ARROW.
-                    token = synchronize(ARROW_SET);
-
-                    if (token->get_type() == (TokenType) PT_ARROW)
-                    {
-                        token = next_token(token);  // consume the ARROW
-                    }
-                    else {
-                        // TODO: Add error handling for ARROW
-                        error_handler.flag(token, MISSING_THEN, this);
-                    }
-
-                    // Parse the compound statement.
-                    // The IF node adopts the statement subtree as its second child.
-                    StatementParser statement_parser(this);
-                    if_node->add_child(statement_parser.parse_statement(token));
-                    token = current_token();
-
-                    token = next_token(token); // EAT THE FUCKING SEMICOLON
-
-
-                    // TODO: Move this into a function that takes parent if node and add the results to its third child
-                    // Currently not working because original IF node has too many children
-                    while(token->get_type() != (TokenType) PT_END && token->get_type() != (TokenType) PT_OTHERWISE)
-                    {
-                        // Parse expression
-                        // The IF node adopts the expression subtree as its first child.
-                        if_node->add_child(expression_parser.parse_statement(token));
-
-                        // Synchronize at the ARROW.
-                        token = synchronize(ARROW_SET);
-
-                        if (token->get_type() == (TokenType) PT_ARROW)
-                        {
-                            token = next_token(token);  // consume the ARROW
-                        }
-                        else {
-                            // TODO: Add error handling for ARROW
-                            error_handler.flag(token, MISSING_THEN, this);
-                        }
-
-                        // Parse the compound statement.
-                        // The IF node adopts the statement subtree as its second child.
-                        if_node->add_child(statement_parser.parse_statement(token));
-                        token = current_token();
-
-                        token = next_token(token); // EAT THE FUCKING SEMICOLON
-
-                        // Look for an OTHERWISE.
-                        if (token->get_type() == (TokenType) PT_OTHERWISE)
-                        {
-                            token = next_token(token);  // consume the OTHERWISE
-
-                            // Synchronize at the ARROW.
-                            token = synchronize(ARROW_SET);
-
-                            if (token->get_type() == (TokenType) PT_ARROW)
-                            {
-                                token = next_token(token);  // consume the ARROW
-                            }
-                            else {
-                                // TODO: Add error handling for ARROW
-                                error_handler.flag(token, MISSING_THEN, this);
-                            }
-
-                            // Parse the compound statement.
-                            // The IF node adopts the statement subtree as its third child.
-                            if_node->add_child(statement_parser.parse_statement(token));
-
-                        }
-                    }
-
-                    token = next_token(token); // consume END
-
-                    return if_node;
-                }
-
-            }}}}  // namespace wci::frontend::pascal::parsers
+}}}}  // namespace wci::frontend::pascal::parsers
